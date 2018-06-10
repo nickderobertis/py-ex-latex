56a7e93fe14cd6015a542e4f4dd9fb1c8cecdaf0
Nick DeRobertis <whoopnip@gmail.com>
2018-06-10 18:49:13 -0400

    Add convenience method show_contents to imports

--- dero/latex/table/__init__.py
+++ dero/latex/table/__init__.py
@@ -2,3 +2,4 @@ from dero.latex.table.logic import output_panel_collection_to_tex
 from dero.latex.table.models.panels import PanelCollection, Panel
 from dero.latex.table.models.data.table import DataTable
 from dero.latex.table.models.labels.table import LabelCollection, LabelTable
+from dero.latex.logic.tools import show_contents
