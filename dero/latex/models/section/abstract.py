31f34e95d87ec6f2a2833074e29563fcbc17fca4
Nick DeRobertis <nick@claimfound.com>
2019-03-19 11:58:42 -0400

    Latex: Properly format contents in abstract

--- dero/latex/models/section/abstract.py
+++ dero/latex/models/section/abstract.py
@@ -1,9 +1,11 @@
 from dero.latex.models.item import Item
 from dero.latex.models.documentitem import DocumentItem
+from dero.latex.logic.format.contents import format_contents
 
 
 class Abstract(DocumentItem, Item):
     name = 'abstract'
 
     def __init__(self, content):
+        content = format_contents(content)
         super().__init__(self.name, content)
