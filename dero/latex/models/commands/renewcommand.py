a02ac2b382984c71606cd57bb6b45138ed510d7e
Nick DeRobertis <nick@claimfound.com>
2019-03-19 12:51:52 -0400

    Latex: Add model for redefining commands

---
+++ dero/latex/models/commands/renewcommand.py
@@ -0,0 +1,8 @@
+from dero.latex.models.item import MultiOptionSimpleItem
+
+
+class ReNewCommand(MultiOptionSimpleItem):
+    name = 'renewcommand'
+
+    def __init__(self, command_name: str, command_definition: str):
+        super().__init__(self.name, rf'\{command_name}', command_definition)
