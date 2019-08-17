220a55eb9c0724e540fbc115994a2f3264120710
Nick DeRobertis <nick@claimfound.com>
2019-08-17 10:36:56 -0400

    Latex: Add adjust box

---
+++ dero/latex/models/format/adjustbox.py
@@ -0,0 +1,20 @@
+from typing import Sequence
+from dero.latex.models.item import Item
+from dero.latex.models.section.base import TextAreaMixin
+from dero.latex.models.package import Package
+
+
+class AdjustBox(TextAreaMixin, Item):
+    name = 'adjustbox'
+
+    def __init__(self, contents, adjust_options: Sequence[str]):
+        self.adjust_options = adjust_options
+        self.init_data()
+        self.data.packages.append(Package('adjustbox'))
+        super().__init__(self.name, contents, env_modifiers=self.options_str)
+
+    @property
+    def options_str(self) -> str:
+        base_str = ', '.join(self.adjust_options)
+        return self._wrap_with_braces(base_str)
+
