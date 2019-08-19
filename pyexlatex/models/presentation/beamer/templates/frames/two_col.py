b80319b07fcc36a35c8892241956d7ff3b6741d2
Nick DeRobertis <nick@claimfound.com>
2019-08-19 13:58:18 -0400

    Update TwoColumnGraphicFrame template for better alignment of multiple graphics

--- pyexlatex/models/presentation/beamer/templates/frames/two_col.py
+++ pyexlatex/models/presentation/beamer/templates/frames/two_col.py
@@ -2,10 +2,9 @@ from typing import Sequence, List, Union
 from pyexlatex.models.presentation.beamer.templates.frames.grid import AutoSizeGridFrame
 from pyexlatex.figure.models.graphic import Graphic
 from pyexlatex.models.format.vfill import VFill
-from pyexlatex.models.presentation.beamer.templates.lists.dim_reveal_items import DimAndRevealListItems
 from pyexlatex.models.presentation.beamer.templates.frames.dim_reveal import DimRevealMixin
-from pyexlatex.models.lists.ordered import OrderedList
-from pyexlatex.models.lists.unordered import UnorderedList
+from pyexlatex.models.sizes.textheight import TextHeight
+from pyexlatex.models.format.centering import Centering
 
 
 class TwoColumnFrame(AutoSizeGridFrame):
@@ -24,9 +23,16 @@ class TwoColumnGraphicFrame(TwoColumnFrame):
 
     @property
     def graphic_contents(self) -> List[Union[Graphic, VFill]]:
-        graphic_contents = [VFill()]
+        graphic_contents = [Centering(), VFill()]
         for filepath in self.graphic_filepaths:
-            graphic_contents.append(Graphic(filepath))
+            graphic_contents.append(Graphic(
+                filepath,
+                width=0.9,
+                options=[
+                    f'height={self.graphic_height}',
+                    'keepaspectratio'
+                ]
+            ))
             graphic_contents.append(VFill())
         graphic_contents.append(VFill())
 
@@ -45,6 +51,19 @@ class TwoColumnGraphicFrame(TwoColumnFrame):
             return self.graphic_contents
         else:
             return self.content
+
+    @property
+    def graphic_spacing(self) -> float:
+        """
+        Total amount of padding between images as a fraction of text height
+        """
+        return (len(self.graphic_filepaths) - 1) * 0.20
+
+    @property
+    def graphic_height(self) -> str:
+        total_area = 1 - self.graphic_spacing
+        per_graphic = total_area/len(self.graphic_filepaths)
+        return f'{per_graphic}{TextHeight()}'
 
 
 class TwoColumnGraphicDimRevealFrame(DimRevealMixin, TwoColumnGraphicFrame):
