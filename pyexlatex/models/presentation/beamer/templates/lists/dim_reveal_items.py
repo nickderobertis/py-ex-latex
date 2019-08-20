b2f5fa4c8d00f59d35989714e39e32e7652e27d3
Nick DeRobertis <nick@claimfound.com>
2019-08-19 20:01:50 -0400

    Add function for removing dim and reveal items from output

--- pyexlatex/models/presentation/beamer/templates/lists/dim_reveal_items.py
+++ pyexlatex/models/presentation/beamer/templates/lists/dim_reveal_items.py
@@ -12,6 +12,7 @@ from pyexlatex.models.item import ItemBase
 class DimAndRevealListItem(ListItem):
 
     def __init__(self, contents, dim: bool = True, opacity: float = 0.3, **kwargs):
+        self.dim = dim
         dim_ov = Overlay([UntilEnd(NextWithoutIncrement(1))])
         next_ov = Overlay([UntilEnd(NextWithIncrement())])
 
@@ -19,6 +20,17 @@ class DimAndRevealListItem(ListItem):
             contents = TextColor(contents, 'black', opacity=opacity, overlay=dim_ov)
 
         super().__init__(contents, overlay=next_ov, **kwargs)
+
+    def convert_to_regular_item(self):
+        if self.dim:
+            contents = self.contents.content  # get from inside TextColor
+        else:
+            contents = self.contents
+        item = ListItem(contents)
+        self.__dict__.update(item.__dict__)
+        self.__class__ = ListItem
+        del self.is_DimAndRevealListItem
+        del self.dim
 
 
 class DimAndRevealListItems(VerticalFillMixin, ContainerItem, ItemBase):
@@ -49,3 +61,17 @@ class DimAndRevealListItems(VerticalFillMixin, ContainerItem, ItemBase):
         if isinstance(self.contents, str):
             return self.contents
         return _build(self.contents)
+
+
+def eliminate_dim_reveal(content):
+    """
+    Eliminates dim/reveal from nested content. Modifies in place by using regular list items
+    """
+    if hasattr(content, 'content'):
+        eliminate_dim_reveal(content.content)
+    if hasattr(content, 'contents'):
+        eliminate_dim_reveal(content.contents)
+    if hasattr(content, 'is_DimAndRevealListItem') and content.is_DimAndRevealListItem:
+        content.convert_to_regular_item()
+    if isinstance(content, (list, tuple)):
+        [eliminate_dim_reveal(c) for c in content]
