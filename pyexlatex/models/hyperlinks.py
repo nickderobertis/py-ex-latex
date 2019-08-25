59e7908dbe3182704fefc27d44f67b7484cae455
Nick DeRobertis <nick@claimfound.com>
2019-08-25 09:30:30 -0400

    Add hyperlinks

---
+++ pyexlatex/models/hyperlinks.py
@@ -0,0 +1,28 @@
+from typing import Optional
+from pyexlatex.models.item import MultiOptionSimpleItem
+from pyexlatex.models.section.base import TextAreaMixin
+from pyexlatex.models.package import Package
+
+
+class Hyperlink(TextAreaMixin, MultiOptionSimpleItem):
+    """
+    Create links to urls, with or without displaying text instead of the link.
+    """
+
+    def __init__(self, href: str, content: Optional = None, **kwargs):
+        self.href = href
+        self.content = content
+
+        if content is None:
+            self.name = 'url'
+            options = (href,)
+        else:
+            self.name = 'href'
+            options = (href, content)
+
+        self.add_data_from_content(content)
+        self.data.packages.append(
+            Package('hyperref')
+        )
+
+        MultiOptionSimpleItem.__init__(self, self.name, *options, **kwargs)
