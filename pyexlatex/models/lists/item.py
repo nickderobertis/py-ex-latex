86d68f02e849dcb48443a77b7f93f2fbbfffe8b6
Nick DeRobertis <nick@claimfound.com>
2019-08-25 09:22:16 -0400

    Fix bug where list item was not handling text contents appropriately

--- pyexlatex/models/lists/item.py
+++ pyexlatex/models/lists/item.py
@@ -2,11 +2,11 @@ from typing import Optional, TYPE_CHECKING
 if TYPE_CHECKING:
     from pyexlatex.models.presentation.beamer.overlay.overlay import Overlay
 from pyexlatex.models.item import NoBracesItem
-from pyexlatex.models.containeritem import ContainerItem
+from pyexlatex.models.section.base import TextAreaMixin
 from mixins.repr import ReprMixin
 
 
-class ListItem(ContainerItem, NoBracesItem, ReprMixin):
+class ListItem(TextAreaMixin, NoBracesItem, ReprMixin):
     name = 'item'
     repr_cols = ['contents']
 
