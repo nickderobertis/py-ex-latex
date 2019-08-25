13b8c04d7d5d6824d84315a213bf4af137097d0d
Nick DeRobertis <nick@claimfound.com>
2019-08-25 09:46:40 -0400

    Add docstring for text

--- pyexlatex/models/format/text.py
+++ pyexlatex/models/format/text.py
@@ -3,6 +3,9 @@ from pyexlatex.models.item import SimpleItem
 
 
 class Text(SimpleItem):
+    """
+    Gets back to raw text, useful to have normal text formatting inside of equations.
+    """
     name = 'text'
 
     def __init__(self, env: str, modifiers: Optional[str] = None):
