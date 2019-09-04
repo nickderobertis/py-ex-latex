772b17484b2533b2dfc114b5c7958e4744a07d8a
Nick DeRobertis <nick@claimfound.com>
2019-09-04 15:07:56 -0400

    Fix error in passing latex items to contact lines

--- pyexlatex/resume/models/contactline.py
+++ pyexlatex/resume/models/contactline.py
@@ -9,6 +9,7 @@ class ContactLine(TextAreaMixin, SimpleItem):
 
     def __init__(self, contact_info: Union[str, Sequence[str]]):
         self.contact_info = contact_info
+        self.add_data_from_content(contact_info)
 
         super().__init__(self.name, self._get_content())
 
@@ -17,4 +18,4 @@ class ContactLine(TextAreaMixin, SimpleItem):
             return self.contact_info
 
         separator = f' {OutputLineBreak()} '
-        return separator.join(self.contact_info)
+        return separator.join([str(item) for item in self.contact_info])
