0f23004b021dcd12c656eca672c7f7aad75180d5
Nick DeRobertis <whoopnip@gmail.com>
2019-09-11 18:42:07 -0400

    Add this page style model

--- pyexlatex/models/page/style.py
+++ pyexlatex/models/page/style.py
@@ -2,7 +2,20 @@ from pyexlatex.models.item import SimpleItem
 
 
 class PageStyle(SimpleItem):
+    """
+    Sets style of the pages for the entire document.
+    """
     name = 'pagestyle'
+
+    def __init__(self, contents):
+        super().__init__(self.name, contents)
+
+
+class ThisPageStyle(SimpleItem):
+    """
+    Sets style of the current page in a document.
+    """
+    name = 'thispagestyle'
 
     def __init__(self, contents):
         super().__init__(self.name, contents)
