b503b03a76d5beb513ba3280b651bfacbf9f5a2e
Nick DeRobertis <whoopnip@gmail.com>
2019-09-18 08:01:11 -0400

    Add optional description to publication

--- pyexlatex/resume/templates/publication.py
+++ pyexlatex/resume/templates/publication.py
@@ -2,19 +2,22 @@ from typing import Optional, Sequence, Union, List
 import pyexlatex as pl
 from pyexlatex.logic.format.and_join import join_with_commas_and_and_output_list
 from pyexlatex.models.format.hangindent import HangIndent
+from pyexlatex.models.format.paragraph.justifying import Justifying
 
 
 class Publication(pl.Template):
     SPACE_BETWEEN_ADJUSTMENT = 0.2
 
     def __init__(self, title: str, co_authors: Optional[Sequence[str]] = None, journal_info: Optional[str] = None,
-                 href: Optional[str] = None, extra_info: Optional[str] = None, prevent_page_break: bool = True):
+                 href: Optional[str] = None, extra_info: Optional[str] = None, prevent_page_break: bool = True,
+                 description: Optional = None):
         self.title = title
         self.co_authors = co_authors
         self.journal_info = journal_info
         self.href = href
         self.extra_info = extra_info
         self.prevent_page_break = prevent_page_break
+        self.description = description
 
         self.contents = self._get_contents()
         super().__init__()
@@ -26,7 +29,8 @@ class Publication(pl.Template):
             pl.Italics(self.journal_info) if self.journal_info is not None else None,
             self._with_coauthors,
             self.extra_info,
-            pl.VSpace(self.SPACE_BETWEEN_ADJUSTMENT)
+            *self._description_content,
+            pl.VSpace(self.SPACE_BETWEEN_ADJUSTMENT),
         ]
         items = [item for item in possible_items if item is not None]
         if self.prevent_page_break:
@@ -59,3 +63,15 @@ class Publication(pl.Template):
             return None
 
         return [' with ', *join_with_commas_and_and_output_list(self.co_authors)]
+
+    @property
+    def _description_content(self) -> List:
+        if self.description is None:
+            return []
+
+        return [
+            '',  # separate paragraph
+            Justifying(),  # return to justifying on both sides
+            self.description
+        ]
+
