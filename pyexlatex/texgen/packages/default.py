69321dac0146e3a94dcedffd228f9782be555f4c
Nick DeRobertis <whoopnip@gmail.com>
2019-11-01 23:00:23 -0400

    Keep table packages in order

--- pyexlatex/texgen/packages/default.py
+++ pyexlatex/texgen/packages/default.py
@@ -3,4 +3,6 @@ from pyexlatex.table.models.texgen.packages import default_packages as default_t
 
 # exclude geometry package as has different margins in figure vs table. Use table default.
 figure_packages = [package for package in default_figure_packages if not package.matches_name('geometry')]
-default_packages = list(set(default_table_packages + figure_packages))
+
+# Remove duplicates, keeping order
+default_packages = list({package: '' for package in default_table_packages + figure_packages}.keys())
