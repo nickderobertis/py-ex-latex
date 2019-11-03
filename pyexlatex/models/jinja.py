547010452b8a9877d064d59b8d25fc64def87435
Nick DeRobertis <whoopnip@gmail.com>
2019-11-03 09:58:44 -0500

    Set jinja environment template class to custom template, allowing templates from environments to work the same

--- pyexlatex/models/jinja.py
+++ pyexlatex/models/jinja.py
@@ -19,28 +19,6 @@ def get_capitalized_items(items: Sequence[str]) -> List[str]:
     return [name for name in items if UPPER_PATTERN.match(name[0])]
 
 
-class JinjaEnvironment(Environment):
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self._add_filters()
-
-    def _add_filters(self):
-        import pyexlatex as pl
-        import pyexlatex.table as lt
-        import pyexlatex.presentation as lp
-        import pyexlatex.graphics as lg
-        import pyexlatex.layouts as ll
-        import pyexlatex.figure as lf
-
-        for module in [pl, lt, lp, lg, ll, lf]:
-            latex_class_names = get_capitalized_items(dir(module))
-            for latex_class_name in latex_class_names:
-                latex_class = getattr(module, latex_class_name)
-                this_class_factory = partial(class_factory, latex_class)
-                self.filters[latex_class_name] = this_class_factory
-
-
 class JinjaTemplate(Template, ContainerItem):
 
     def __new__(cls, source, **kwargs):
@@ -66,3 +44,26 @@ class DataString(ItemBase):
 
     def __str__(self):
         return self.content
+
+
+class JinjaEnvironment(Environment):
+    template_class = JinjaTemplate
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._add_filters()
+
+    def _add_filters(self):
+        import pyexlatex as pl
+        import pyexlatex.table as lt
+        import pyexlatex.presentation as lp
+        import pyexlatex.graphics as lg
+        import pyexlatex.layouts as ll
+        import pyexlatex.figure as lf
+
+        for module in [pl, lt, lp, lg, ll, lf]:
+            latex_class_names = get_capitalized_items(dir(module))
+            for latex_class_name in latex_class_names:
+                latex_class = getattr(module, latex_class_name)
+                this_class_factory = partial(class_factory, latex_class)
+                self.filters[latex_class_name] = this_class_factory
