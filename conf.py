5481c079a0d44ee491ff6898233feddf1a155741
Nick DeRobertis <whoopnip@gmail.com>
2020-02-17 11:30:57 -0500

    Update package version

--- conf.py
+++ conf.py
@@ -17,7 +17,7 @@ REPO_MAINTAINERS = [
 ]
 
 # Package version in the format (major, minor, release)
-PACKAGE_VERSION_TUPLE = (0, 10, 3)
+PACKAGE_VERSION_TUPLE = (0, 10, 4)
 
 # Short description of the package
 PACKAGE_SHORT_DESCRIPTION = 'Python Extends LaTeX - A High-Level Python API for Creating Latex Documents'
