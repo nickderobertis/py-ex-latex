4848ff658133e1d27633d9ab277746629ecbc5db
Nick DeRobertis <whoopnip@gmail.com>
2020-02-18 13:54:06 -0500

    Import sympy classes directly to avoid autodoc typehints warning for forward annotations

--- pyexlatex/logic/format/eq.py
+++ pyexlatex/logic/format/eq.py
@@ -1,18 +1,16 @@
-from typing import TYPE_CHECKING
-if TYPE_CHECKING:
-    from sympy import Eq, Expr, Symbol
+from sympy import Eq, Expr, Symbol
 from sympy import latex
 
 
-def inline(eq: 'Eq') -> str:
+def inline(eq: Eq) -> str:
     return f'${latex(eq)}$'
 
 
-def offset(eq: 'Eq') -> str:
+def offset(eq: Eq) -> str:
     return f'\\begin{{equation}}\n\t{latex(eq)}\n\\end{{equation}}'
 
 
-def latex_partial(expr: 'Expr', eq_symbol: 'Symbol', wrt_symbol: 'Symbol', offset: bool = False) -> str:
+def latex_partial(expr: Expr, eq_symbol: Symbol, wrt_symbol: Symbol, offset: bool = False) -> str:
 
     begin_str = '$'
     end_str = '$'
