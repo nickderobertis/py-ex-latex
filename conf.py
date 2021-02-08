89e38e82fd22b2f665e3bdff121b066388689298
Nick DeRobertis <whoopnip@gmail.com>
2021-02-08 14:33:54 -0500

    Refactor logic which gets packages for table and figure size adjustment in Document into separate function so it can be used elsewhere

--- conf.py
+++ conf.py
@@ -20,7 +20,7 @@ REPO_MAINTAINERS = [
 ]
 
 # Package version in the format (major, minor, release)
-PACKAGE_VERSION_TUPLE = (0, 13, 10)
+PACKAGE_VERSION_TUPLE = (0, 13, 11)
 
 # Short description of the package
 PACKAGE_SHORT_DESCRIPTION = 'Python Extends LaTeX - A High-Level Python API for Creating Latex Documents'
